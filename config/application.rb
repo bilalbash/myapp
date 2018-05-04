2dfe8a10f55510767425a8ae20e3380a6f617a61
bilalbash <bilal.basharat@gmail.com>
2018-05-04 11:12:03 +0500

    rspec generator settings

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,9 @@ module Myapp
         fixtures: true,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
